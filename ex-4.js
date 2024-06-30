b8e0f3e16b1fd65fcdb4866e4ec53a3109326e1d
petandnon <petandnon@gmail.com>
2024-06-30 21:34:27 +0700

    Sent assignment

--- ex-4.js
+++ ex-4.js
@@ -3,3 +3,19 @@
 let lightBulbStatus = "On";
 
 //Start coding here
+let message ;
+switch(lightBulbStatus){
+    case "On":
+        message = "Light Bulb is On."
+        break
+    case "Off":
+        message = "Light Bulb is Off."
+        break
+    case "Broken"    :
+        message = "Light Bulb is Broken."
+        break
+    default:
+        message = "Please choose the correct input (On/Off/Broken)."
+        break    
+}  
+console.log(message)
